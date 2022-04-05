ce549376d1663b5079bd39027fa12df7e07a5d83
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2022-04-05 20:28:56 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,40 @@ import { useState } from 'react';
 import PhotosByTag from './components/PhotosByTag';
 import Tags from './components/Tags';
 
+const tagsList = [
+  { title: "TAG 1", color: "blue" },
+  { title: "TAG 2", color: "red" },
+  { title: "TAG 3", color: "grey" }
+];
+
+
 function App() {
   const [photos, setPhotos] = useState([]);
   const [taggedPhotos, setTaggedPhotos] = useState([]);
-  const [tags, setTags] = useState();
+  const [tags, setTags] = useState(tagsList);
   const [currentPhoto, setCurrentPhoto] = useState("");
   const [currentTag, setCurrentTag] = useState("");
 
   return (
     <div className="App">
       <Tags
-      tags={tags} setTags={setTags}
+      tags={tags}
+      setTags={setTags}
+      photos={photos}
+      setPhotos={setPhotos}
+      currentPhoto={currentPhoto} 
+      setCurrentPhoto={setCurrentPhoto}
+      taggedPhotos={taggedPhotos}
+      setTaggedPhotos={setTaggedPhotos}
+      currentTag={currentTag}
+      setCurrentTag={setCurrentTag}
       ></Tags>
       <Pics></Pics>
-      <PhotosByTag></PhotosByTag>
+      <PhotosByTag
+      tags={tags}
+      taggedPhotos={taggedPhotos}
+      setTaggedPhotos={setTaggedPhotos}
+      ></PhotosByTag>
     </div>
   );
 }
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,8 +1,9 @@
 import {useState} from 'react'
 import { randomColor } from "randomcolor";
+import './tags.css'
 
-
-export default function Tags({tags, setTags}) {
+export default function Tags({tags, setTags, photos, setPhotos, currentPhoto, SetCurrentPhoto, taggedPhotos, setTaggedPhotos, currentTag, setCurrentTag
+}) {
     const [newTag, setNewTag] = useState("");
     const addNewTag = (tagName) => {
         if (tags.some((tag) => tag.label === tagName) || !tagName) {
@@ -13,6 +14,31 @@ export default function Tags({tags, setTags}) {
         setTags([...tags, tagObj]);
         setNewTag("");
       };
+      const title = currentPhoto
+      ? `photo ${currentPhoto?.userId} - Assigned tags`
+      : "Available Tags";
+
+      const style = { border: `1px solid ${currentTag.color}` };
+
+      const editTagName = (tagName) => {};
+
+      const deleteTag = (tagName) => {
+        const temp = [...tags].filter((tag) => tag.label !== tagName);
+        setTags(temp);
+      };
+
+      const tagPhoto = (tagName) => {
+        currentPhoto.tag = tagName.label;
+        const temp = [...photos].filter((photo) => photo.id !== currentPhoto.id);
+        setTaggedPhotos([...taggedPhotos, currentPhoto]);
+        setPhotos(temp);
+        // setCurrentPhoto("");
+        setCurrentTag("");
+      };
+    
+    
+
+  
     
 
   return (
@@ -33,6 +59,53 @@ export default function Tags({tags, setTags}) {
         />
         <input type="submit" value="save" />
       </form>
+      <div className="TagsList">
+        <p>{title}</p>
+        {tags.map((tag) => {
+          return (
+            <li
+              style={
+                (currentTag?.title === tag.title && style) || {
+                  background: tag.color
+                }
+              }
+              key={tag.title}
+              onClick={() => setCurrentTag(tag)}
+              onDoubleClick={() => setCurrentTag("")}
+            >
+              {tag.label}
+              {/* <span>
+                {
+                  <AiOutlineEdit
+                    title="Rename"
+                    onClick={() => editTagName(tag.label)}
+                  />
+                }
+                {
+                  <AiFillDelete
+                    title="Delete"
+                    onClick={() => deleteTag(tag.label)}
+                  />
+                }
+              </span> */}
+            </li>
+            /*: <input type="text" value={tag.label}/>*/
+            /*: <input type="text" value={tag.label}/>*/
+            /*: <input type="text" value={tag.label}/>*/
+            /*: <input type="text" value={tag.label}/>*/
+            /*: <input type="text" value={tag.label}/>*/
+           /*: <input type="text" value={tag.label}/>*/);
+        })}
+        {currentTag && currentPhoto ? (
+          <button
+            onClick={() => {
+              tagPhoto(currentTag);
+            }}
+          >
+            Apply
+          </button>
+        ) : null}
+      </div>
 
     </div>
   )
